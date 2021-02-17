98538eaa0f8f2f36bb116f906995217be8447560
valdir-alves3000 <51173956+valdir-alves3000@users.noreply.github.com>
2021-02-17 09:49:00 -0300

    Add files via upload

---
+++ src/database/migrations/20210213101223_create_transactions.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('transactions', function(table) {
+    table.string('id').notNullable();  
+    table.string('description').notNullable();
+    table.string('amount').notNullable();
+    table.string('date').notNullable();
+    table.string('expense').notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('transactions');
+};
